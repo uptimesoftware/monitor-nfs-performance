349fa0474612e2b20acdfc829f9e5c90d7aedd3e
a-salim <salim.abdullah@gmail.com>
2014-03-27 16:42:59 -0400

    fixed for windows monitoring station; 7.2 release

--- pkg/files-posix/scripts/NFS_Performance_Monitor/NFS_Performance_Monitor.sh
+++ pkg/files-posix/scripts/NFS_Performance_Monitor/NFS_Performance_Monitor.sh
@@ -2,4 +2,4 @@
 inst=`grep pidfile /etc/init.d/uptime_core | head -n 1 | cut -d: -f2 | rev | cut -c 12- | rev | sed -e 's/^[ \t]*//'`
 MIBDIRS=$inst/mibs
 export MIBDIRS
-/usr/local/uptime/apache/bin/php ../../plugins/scripts/monitor-nfs-performance/monitor-nfs-performance.php
+../../apache/bin/php NFS_Performance_Monitor.php
--- pkg/files-win/scripts/monitor-nfs-performance/monitor-nfs-performance.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-set PHPDIR=..\..\apache\php\
-"%PHPDIR%\php.exe" ..\..\plugins\scripts\monitor-nfs-performance\monitor-nfs-performance.php
